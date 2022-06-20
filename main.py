2feb4bc61420ef93554ab4cd44a2673ef37f2f0e
Yuran Fang <y7fang@uwaterloo.ca>
2022-06-20 17:42:16 -0400

    weight calculation done

--- main.py
+++ main.py
@@ -6,16 +6,24 @@ def main():
     data = []
     for i in range(21):
         x_i = -2 + 0.2 * i
-        line = []
         for j in range(21):
             x_j = -2 + 0.2 * j
             v = (x_i, x_j)
-            line.append(v)
-        data.append(line)
+            data.append(v)
 
     data = np.array(data)
-    print(type(data))
-    print(data)
+    print(len(data))
+    # print(data)
+
+    sigma = 0.2
+
+    G_ = G(data, sigma)
+    G_intermediate = np.matmul(G_, np.transpose(G_))
+    G_plus = np.matmul(np.power(G_intermediate, -1), np.transpose(G_))
+    print(G_plus.shape)    
+    print(G_plus)
+
+
 
 
 def f(x1, x2):
@@ -32,6 +40,22 @@ def gaussainKernel(X, vertex, sigma):
     euclidean = -(pow((x_i - x_1), 2) + pow((x_j - x_2), 2))
 
     return np.exp(euclidean / (2 * pow(sigma, 2)))
+
+def G(data, sigma):
+    G = []
+    for v in data:
+        line = []
+        for x in data:
+            g = gaussainKernel(x, v, sigma)
+            line.append(g)
+        G.append(line)
+    G = np.array(G)
+    print(G.shape)
+
+    return G
+
+
+
 
 
 
